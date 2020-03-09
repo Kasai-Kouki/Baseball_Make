5d4033f885c1bdec15d5744ddbc89d3a4e2c12c1
Kasai-Kouki <kko_7230@yahoo.co.jp>
2020-03-09 06:34:10 +0000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module BaseballMake
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.hosts << ".amazonaws.com"
-    config.hosts << "salty-reef-05028.herokuapp.com"
+    config.hosts << "baseballmake.herokuapp.com"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
